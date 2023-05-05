f7e0488f7782c4fe00bd9367f9ce64a3c606d702
Eric Xu <eric.r.xu@gmail.com>
2023-05-05 00:25:39 -0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -9,7 +9,7 @@ sudo apt-get install python3-setuptools
 cd klaviyo-weather-app;sudo apt-get install python3-venv
 sudo apt install build-essential libssl-dev libffi-dev python-dev-is-python3
 python3 -m venv env
-source ./env/bin/activate
+. ./env/bin/activate
 python -m pip install --upgrade pip
 sudo apt-get install libsasl2-dev libsasl2-2 libsasl2-modules-gssapi-mit
 sudo apt-get install libmysqlclient-dev
