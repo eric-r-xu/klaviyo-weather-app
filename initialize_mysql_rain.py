a10b4354a4968615acc867c35c7c0a281250786b
Eric Xu <eric.r.xu@gmail.com>
2023-05-05 02:39:06 -0700

    Update initialize_mysql_rain.py

--- initialize_mysql_rain.py
+++ initialize_mysql_rain.py
@@ -10,24 +10,24 @@ def getSQLConn(host, user, password):
 createSchema = "CREATE SCHEMA IF NOT EXISTS rain;"
 
 createTblFactLatLongRain = """CREATE TABLE IF NOT EXISTS rain.TblFactLatLongRain  
-( `timestampChecked` INT(11) NOT NULL,
- `localDateTimeChecked` VARCHAR(255) NOT NULL,
- `timestampUpdated` INT(11) NOT NULL,
- `localDateTimeUpdated` VARCHAR(255) NOT NULL,
-`latitude` DECIMAL(5,4) NOT NULL DEFAULT '0.0000',
-`longitude` DECIMAL(5,4) NOT NULL DEFAULT '0.0000',
-`rain_mm_l90d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l60d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l30d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l7d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l3d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l1d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l3h` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-`rain_mm_l1h` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
-PRIMARY KEY (`timestampChecked`,`timestampUpdated`,`latitude`,`longitude`),
-KEY `idxLatitude` (`idxLatitude`),
-KEY `idxLongitude` (`idxLongitude`)) 
-ENGINE=InnoDB DEFAULT CHARSET=latin1;"""
+    (`timestampChecked` INT(11) NOT NULL,
+    `localDateTimeChecked` VARCHAR(255) NOT NULL,
+    `timestampUpdated` INT(11) NOT NULL,
+    `localDateTimeUpdated` VARCHAR(255) NOT NULL,
+    `latitude` DECIMAL(5,4) NOT NULL DEFAULT '0.0000',
+    `longitude` DECIMAL(5,4) NOT NULL DEFAULT '0.0000',
+    `rain_mm_l90d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l60d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l30d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l7d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l3d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l1d` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l3h` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    `rain_mm_l1h` DECIMAL(5,1) NOT NULL DEFAULT '0.0',
+    PRIMARY KEY (`timestampChecked`,`timestampUpdated`,`latitude`,`longitude`),
+    KEY `idxLatitude` (`idxLatitude`),
+    KEY `idxLongitude` (`idxLongitude`)) 
+    ENGINE=InnoDB DEFAULT CHARSET=latin1;"""
 
 mysql_conn = getSQLConn(MYSQL_AUTH["host"], MYSQL_AUTH["user"], MYSQL_AUTH["password"])
 
