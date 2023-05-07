38605cbbb654965954f53c22e9c661379d2e63d2
Eric Xu <eric.r.xu@gmail.com>
2023-05-07 02:52:48 -0700

    Update initialize_mysql_rain.py

--- initialize_mysql_rain.py
+++ initialize_mysql_rain.py
@@ -1,7 +1,9 @@
 import pymysql
 import warnings
+import pandas as pd
 from local_settings import *
 
+preload = 1
 # connect to sql
 def getSQLConn(host, user, password):
     return pymysql.connect(host=host, user=user, passwd=password, autocommit=True)
@@ -12,13 +14,13 @@ createSchema = "CREATE SCHEMA IF NOT EXISTS rain;"
 createTblFactLatLon = """CREATE TABLE IF NOT EXISTS rain.tblFactLatLon
     (`dt` INT(11) NOT NULL COMMENT 'unixtimestamp of last api data update',
     `updated_pacific_time` VARCHAR(255) NOT NULL COMMENT 'Pacific timezone datetime of last api data update',
-    `requested_pacific_time` VARCHAR(255) NOT NULL COMMENT 'Pacific timezone datetime of last api data request',
+    `requested_pacific_time` VARCHAR(255) COMMENT 'Pacific timezone datetime of last api data request',
     `location_name` VARCHAR(255) NOT NULL COMMENT 'label given for latitude longitude coordinate (e.g. bedwell bayfront park)',
     `lat` DECIMAL(7,3) SIGNED NOT NULL COMMENT 'latitude coordinate',
     `lon` DECIMAL(7,3) SIGNED NOT NULL COMMENT 'longitude coordinate',
     `rain_1h` DECIMAL(5,1) NOT NULL COMMENT 'mm rainfall in last hour',
     `rain_3h` DECIMAL(5,1) NOT NULL COMMENT 'mm rainfall in last 3 hours', 
-    PRIMARY KEY (`timestampChecked`,`timestampUpdated`,`latitude`,`longitude`)) 
+    PRIMARY KEY (`dt`,`lat`,`lon`)) 
     ENGINE=InnoDB DEFAULT CHARSET=latin1;"""
 
 mysql_conn = getSQLConn(MYSQL_AUTH["host"], MYSQL_AUTH["user"], MYSQL_AUTH["password"])
@@ -28,3 +30,34 @@ with mysql_conn.cursor() as cursor:
         warnings.simplefilter("ignore")
         cursor.execute(createSchema)
         cursor.execute(createTblFactLatLon)
+
+
+requested_pacific_time = "2023-05-06"
+query = (
+    "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
+    % (
+        dt,
+        unixtime_to_pacific_datetime(dt),
+        requested_pacific_time,
+        location_name,
+        lat,
+        lon,
+        rain_1h,
+        rain_3h,
+    )
+)
+logging.info("query=%s" % (query))
+runQuery(mysql_conn, query)
+logging.info(
+    "%s - %s - %s - %s - %s - %s - %s - %s"
+    % (
+        dt,
+        unixtime_to_pacific_datetime(dt),
+        requested_pacific_time,
+        location_name,
+        lat,
+        lon,
+        rain_1h,
+        rain_3h,
+    )
+)
