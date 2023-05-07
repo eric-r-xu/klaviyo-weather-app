f0b22e5c044a82411b830c8f2e3a97bdf62d0e02
Eric Xu <eric.r.xu@gmail.com>
2023-05-07 03:34:26 -0700

    Update initialize_mysql_rain.py

--- initialize_mysql_rain.py
+++ initialize_mysql_rain.py
@@ -32,22 +32,32 @@ with mysql_conn.cursor() as cursor:
         cursor.execute(createSchema)
         cursor.execute(createTblFactLatLon)
 
-        
 
-df = pd.read_csv('initial_data.csv').fillna(0)
+def unixtime_to_pacific_datetime(unixtime_timestamp):
+    # Create a timezone object for the Pacific timezone
+    pacific_timezone = pytz.timezone("US/Pacific")
+    # Convert the Unix timestamp to a datetime object in UTC timezone
+    utc_datetime = datetime.datetime.utcfromtimestamp(unixtime_timestamp)
+
+    # Convert the UTC datetime object to the Pacific timezone
+    output = pacific_timezone.localize(utc_datetime).astimezone(pacific_timezone)
+    return str(output)
+
+
+df = pd.read_csv("initial_data.csv").fillna(0)
 requested_pacific_time = "2023-05-06"
 for index, row in df.iterrows():
     query = (
         "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
         % (
-            row['dt'],
-            unixtime_to_pacific_datetime(int(row['dt'])),
+            row["dt"],
+            unixtime_to_pacific_datetime(int(row["dt"])),
             requested_pacific_time,
-            row['location_name'],
-            row['lat'],
-            row['lon'],
-            row['rain_1h'],
-            row['rain_3h'],
+            row["location_name"],
+            row["lat"],
+            row["lon"],
+            row["rain_1h"],
+            row["rain_3h"],
         )
     )
     logging.info("query=%s" % (query))
@@ -55,14 +65,14 @@ for index, row in df.iterrows():
     logging.info(
         "%s - %s - %s - %s - %s - %s - %s - %s"
         % (
-            row['dt'],
-            unixtime_to_pacific_datetime(int(row['dt'])),
+            row["dt"],
+            unixtime_to_pacific_datetime(int(row["dt"])),
             requested_pacific_time,
-            row['location_name'],
-            row['lat'],
-            row['lon'],
-            row['rain_1h'],
-            row['rain_3h'],
+            row["location_name"],
+            row["lat"],
+            row["lon"],
+            row["rain_1h"],
+            row["rain_3h"],
         )
     )
-logging.info("finished preload")   
+logging.info("finished preload")
