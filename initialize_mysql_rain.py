8cc2f9cf2e1d1990c94030830cc947e5252c5dc6
Eric Xu <eric.r.xu@gmail.com>
2023-05-07 03:31:08 -0700

    Update initialize_mysql_rain.py

--- initialize_mysql_rain.py
+++ initialize_mysql_rain.py
@@ -1,6 +1,8 @@
 import pymysql
 import warnings
 import pandas as pd
+import datetime
+import pytz
 from local_settings import *
 
 preload = 1
@@ -31,33 +33,37 @@ with mysql_conn.cursor() as cursor:
         cursor.execute(createSchema)
         cursor.execute(createTblFactLatLon)
 
-
-requested_pacific_time = "2023-05-06"
-query = (
-    "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
-    % (
-        dt,
-        unixtime_to_pacific_datetime(dt),
-        requested_pacific_time,
-        location_name,
-        lat,
-        lon,
-        rain_1h,
-        rain_3h,
-    )
-)
-logging.info("query=%s" % (query))
-runQuery(mysql_conn, query)
-logging.info(
-    "%s - %s - %s - %s - %s - %s - %s - %s"
-    % (
-        dt,
-        unixtime_to_pacific_datetime(dt),
-        requested_pacific_time,
-        location_name,
-        lat,
-        lon,
-        rain_1h,
-        rain_3h,
-    )
-)
+        
+if preload == 1:
+    df = pd.read_csv('initial_data.csv', index = False).fillna(0)
+    requested_pacific_time = "2023-05-06"
+    for index, row in df.iterrows():
+        query = (
+            "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
+            % (
+                row['dt'],
+                unixtime_to_pacific_datetime(int(row['dt'])),
+                requested_pacific_time,
+                row['location_name'],
+                row['lat'],
+                row['lon'],
+                row['rain_1h'],
+                row['rain_3h'],
+            )
+        )
+        logging.info("query=%s" % (query))
+        runQuery(mysql_conn, query)
+        logging.info(
+            "%s - %s - %s - %s - %s - %s - %s - %s"
+            % (
+                row['dt'],
+                unixtime_to_pacific_datetime(int(row['dt'])),
+                requested_pacific_time,
+                row['location_name'],
+                row['lat'],
+                row['lon'],
+                row['rain_1h'],
+                row['rain_3h'],
+            )
+        )
+    logging.info("finished preload")   
