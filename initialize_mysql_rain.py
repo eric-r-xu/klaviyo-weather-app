c18e2d82c3c15384644fa6f1dda92c90a9b84b5b
Eric Xu <eric.r.xu@gmail.com>
2023-05-07 03:32:56 -0700

    Update initialize_mysql_rain.py

--- initialize_mysql_rain.py
+++ initialize_mysql_rain.py
@@ -33,36 +33,36 @@ with mysql_conn.cursor() as cursor:
         cursor.execute(createTblFactLatLon)
 
         
-if preload == 1:
-    df = pd.read_csv('initial_data.csv', index = False).fillna(0)
-    requested_pacific_time = "2023-05-06"
-    for index, row in df.iterrows():
-        query = (
-            "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
-            % (
-                row['dt'],
-                unixtime_to_pacific_datetime(int(row['dt'])),
-                requested_pacific_time,
-                row['location_name'],
-                row['lat'],
-                row['lon'],
-                row['rain_1h'],
-                row['rain_3h'],
-            )
+
+df = pd.read_csv('initial_data.csv', index = False).fillna(0)
+requested_pacific_time = "2023-05-06"
+for index, row in df.iterrows():
+    query = (
+        "INSERT INTO rain.tblFactLatLon(dt, updated_pacific_time, requested_pacific_time, location_name, lat, lon, rain_1h, rain_3h) VALUES (%i, '%s', '%s', '%s', %.3f, %.3f, %.1f, %.1f)"
+        % (
+            row['dt'],
+            unixtime_to_pacific_datetime(int(row['dt'])),
+            requested_pacific_time,
+            row['location_name'],
+            row['lat'],
+            row['lon'],
+            row['rain_1h'],
+            row['rain_3h'],
         )
-        logging.info("query=%s" % (query))
-        runQuery(mysql_conn, query)
-        logging.info(
-            "%s - %s - %s - %s - %s - %s - %s - %s"
-            % (
-                row['dt'],
-                unixtime_to_pacific_datetime(int(row['dt'])),
-                requested_pacific_time,
-                row['location_name'],
-                row['lat'],
-                row['lon'],
-                row['rain_1h'],
-                row['rain_3h'],
-            )
+    )
+    logging.info("query=%s" % (query))
+    runQuery(mysql_conn, query)
+    logging.info(
+        "%s - %s - %s - %s - %s - %s - %s - %s"
+        % (
+            row['dt'],
+            unixtime_to_pacific_datetime(int(row['dt'])),
+            requested_pacific_time,
+            row['location_name'],
+            row['lat'],
+            row['lon'],
+            row['rain_1h'],
+            row['rain_3h'],
         )
-    logging.info("finished preload")   
+    )
+logging.info("finished preload")   
