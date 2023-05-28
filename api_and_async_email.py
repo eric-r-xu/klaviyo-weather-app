3fe741ce2e400fefa476029e3a648c0609daedc3
Eric Xu <eric.r.xu@gmail.com>
2023-05-27 17:00:45 -0700

    Update api_and_async_email.py

--- api_and_async_email.py
+++ api_and_async_email.py
@@ -1,42 +1,38 @@
+import asyncio
+import logging
+import warnings
+
 from flask import Flask
 from flask_mail import Mail, Message
 from datetime import timedelta, datetime
 import aiohttp
-import asyncio
-import logging
 import pytz
 import pymysql
-import warnings
+import pandas as pd
+
 from local_settings import *
 from initialize_mysql import *
+
 
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
-app.config.update(
-    dict(
-        DEBUG=True,
-        MAIL_SERVER="smtp.gmail.com",
-        MAIL_PORT=587,
-        MAIL_USE_TLS=True,
-        MAIL_USE_SSL=False,
-        MAIL_USERNAME=GMAIL_AUTH["mail_username"],
-        MAIL_PASSWORD=GMAIL_AUTH["mail_password"],
-    )
-)
+app.config.from_object("local_settings")
 
 email_service = Mail(app)
+
 
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
+
 async def api_and_email_task(cityID, city_name, dateFact, tomorrow, delay_seconds, email_service, recipients):
     logging.info(f"starting async function `api_and_email_task` for {city_name}")
     logging.info(f"Entering timer of {delay_seconds} seconds")
     await asyncio.sleep(delay_seconds)
-    logging.info(f'Exiting timer of {delay_seconds} seconds')
-    
+    logging.info(f"Exiting timer of {delay_seconds} seconds")
+
     async with aiohttp.ClientSession() as session:
         url = f"http://api.openweathermap.org/data/2.5/weather?id={cityID}&appid={OPENWEATHERMAP_AUTH['api_key']}"
         curr_r = await fetch(session, url)
@@ -54,7 +50,7 @@ async def api_and_email_task(cityID, city_name, dateFact, tomorrow, delay_second
 
         query = "INSERT INTO klaviyo.tblFactCityWeather(city_id, dateFact, today_weather, today_max_degrees_F, tomorrow_max_degrees_F) VALUES (%s, %s, %s, %s, %s)"
         data = (cityID, dateFact, today_weather, today_max_degrees_F, tomorrow_max_degrees_F)
-        runQuery(mysql_conn, query, data)
+        run_query(mysql_conn, query, data)
         logging.info("updated klaviyo.tblactCityWeather")
 
         # Weather conditions and email subject/gifs
@@ -84,11 +80,11 @@ async def api_and_email_task(cityID, city_name, dateFact, tomorrow, delay_second
         messages = []
         for recipient in recipients:
             msg = Message(subject_value, recipients=[recipient], sender=app.config["MAIL_USERNAME"])
-            msg.html = f""" {city_name} - {today_max_degrees_F} degrees F - {today_weather} <br><br><img src="{gif_link}" width="640" height="480"> """
+            msg.html = f"{city_name} - {today_max_degrees_F} degrees F - {today_weather} <br><br><img src='{gif_link}' width='640' height='480'>"
             messages.append(msg)
 
         with app.app_context():
-            with email_service.connect() as conn:
+            async with email_service.connect() as conn:
                 conn.send(messages)
 
         logging.info(f"finished async function `api_and_email_task` for {city_name}")
@@ -108,21 +104,28 @@ async def main():
 
     mysql_conn = getSQLConn(MYSQL_AUTH["host"], MYSQL_AUTH["user"], MYSQL_AUTH["password"])
 
+    def run_query(mysql_conn, query, data=None):
+        with mysql_conn.cursor() as cursor:
+            if data:
+                cursor.execute(query, data)
+            else:
+                cursor.execute(query)
+
     query = """DELETE from klaviyo.tblFactCityWeather where dateFact<date_sub(CURRENT_DATE, interval 60 day) or dateFact=CURRENT_DATE """
-    runQuery(mysql_conn, query)
+    run_query(mysql_conn, query)
     logging.info("finished purging weather data today and older than 60 days from klaviyo.tblFactCityWeather")
 
     query = """DELETE from klaviyo.tblDimEmailCity where sign_up_date<date_sub(CURRENT_DATE, interval 10 day) """
-    runQuery(mysql_conn, query)
+    run_query(mysql_conn, query)
     logging.info("finished purging data older than 10 days from klaviyo.tblDimEmailCity")
 
     tblDimEmailCity = pd.read_sql_query(
         """SELECT group_concat(convert(email,char)) AS email_set, city_id FROM klaviyo.tblDimEmailCity group by city_id""",
         con=mysql_conn,
     )
-    
-    dateFact = str((datetime.now() + timedelta(1)).strftime("%Y-%m-%d"))
-    tomorrow = str((datetime.now() + timedelta(2)).strftime("%Y-%m-%d"))
+
+    dateFact = (datetime.now() + timedelta(1)).strftime("%Y-%m-%d")
+    tomorrow = (datetime.now() + timedelta(2)).strftime("%Y-%m-%d")
 
     for row in tblDimEmailCity.itertuples(index=True, name="Pandas"):
         recipients = str(getattr(row, "email_set")).split(",")
