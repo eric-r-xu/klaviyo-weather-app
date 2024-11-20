a9d7e7327e8656714e414929dcd33d70c19735a1
Eric Xu <eric.r.xu@gmail.com>
2024-11-19 22:37:15 -0800

    Update api_and_email_service_hourly.py

--- api_and_email_service_hourly.py
+++ api_and_email_service_hourly.py
@@ -40,16 +40,14 @@ class ApiAndEmailServiceHourly:
     def run_query(self, query):
         with self.engine.connect() as connection:
             connection.execute(text(query))
-            # Start a transaction
-            transaction = connection.begin()
             try:
                 # Execute the query
                 connection.execute(text(query))
                 # Commit the transaction
-                transaction.commit()
+                connection.commit()
             except Exception as e:
                 # Roll back if something goes wrong
-                transaction.rollback()
+                connection.rollback()
                 raise e
 
     def fetch(self, url):
